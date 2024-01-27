51603617f66d331a42797e5a19933ef667f51fc9
Tommaso Pardi <tommaso.pardi@gmail.com>
2024-01-27 23:27:03 +0000

    feat: setup services

--- db/app.py
+++ db/app.py
@@ -1,14 +1,29 @@
 import pymongo
+from pymongo import MongoClient
+import sys
 
-mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+print("--------EIII")
+try:
+    client = MongoClient('mongodb://mongo_user:mongo_password@mongodb:27017/')
+    db = client["db_test"]
 
-db_test = mongo_client["db_test"]
+    db_users = db["users"]
 
-db_collection = db_test["data"]
+    mydict = { "name": "John", "address": "Highway 37" }
 
-mydict = { "name": "John", "address": "Highway 37" }
-
-x = db_collection.insert_one(mydict)
-
-print('app | ')
-print(db_test.list_collection_names())
+    x = db_users.insert_one(mydict)
+    
+    mydict = { "name": "John", "address": "Highway 38" }
+    
+    x = db_users.insert_one(mydict)
+    
+    db.command("serverStatus")
+    print("MongoDB connection is up.")
+    user_documents = db_users.find({ "name": "John" })
+    
+    for user in user_documents:
+        print(user)
+        
+except Exception as e:
+    print("Error connecting to MongoDB:", e)
+    sys.exit(1)
